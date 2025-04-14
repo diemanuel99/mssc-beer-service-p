1c39a6ceec4b73f3634f2266a36c2d5afaae2c5c
Diego Emanuel Duarte Cardozo <dduarte@roshka.com>
2025-04-14 17:16:21 -0300

    update version of spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.3"
+    id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
 }
-
-var mapstructVersion = "1.6.3"
-var lombokVersion = "1.18.36"
-var lombokMapstructBindingVersion = "0.2.0"
 
 group = "roshka.diegoduarte"
 version = "0.0.1-SNAPSHOT"
@@ -29,6 +25,8 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("roshka.diegoduarte:mssc-brewery-bom:0.0.1-SNAPSHOT"))
+    annotationProcessor(platform("roshka.diegoduarte:mssc-brewery-bom:0.0.1-SNAPSHOT"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -36,18 +34,17 @@ dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    implementation ("org.mapstruct:mapstruct:${mapstructVersion}")
-    implementation ("org.projectlombok:lombok:${lombokVersion}")
+    implementation ("org.mapstruct:mapstruct")
+    implementation ("org.projectlombok:lombok")
 
-    annotationProcessor ("org.mapstruct:mapstruct-processor:${mapstructVersion}")
-    annotationProcessor ("org.projectlombok:lombok:${lombokVersion}")
-    annotationProcessor ("org.projectlombok:lombok-mapstruct-binding:${lombokMapstructBindingVersion}")
-    //testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    annotationProcessor ("org.mapstruct:mapstruct-processor")
+    annotationProcessor ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok-mapstruct-binding")
     // JUnit 5 (Jupiter)
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation ("org.mockito:mockito-core:5.16.0")
-    testImplementation ("org.mockito:mockito-junit-jupiter:5.16.0")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
     //database
     runtimeOnly ("com.h2database:h2")
 
